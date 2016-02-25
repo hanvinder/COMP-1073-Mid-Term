b972a2bba773c0287b340c2698fcb0f6c3380fa2
hanvinder <hanvinder@gmail.com>
2016-02-25 09:38:29 -0500

    Update app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -24,22 +24,99 @@ console.log("App Started...")
 })();
 
 (function () {
-	var thirdParagraph;
+	var Folegandros;
 
-	thirdParagraph = document.getElementById("thirdParagraph");
+	Folegandros = document.getElementById("Folegandros");
 
-	thirdParagraph.innerHTML = "Georgian College is really nice place to learn. They taught , design, and develop engaging and intuitive user interfaces. Courses cover the use of current technologies including but not limited to HTML, CSS, JavaScript, the Document Object Model (DOM), Application Programming Interfaces (APIs), and a variety of popular scripting frameworks.Morover, it's fun to learn here.";
+	Folegandros.innerHTML = "Folegandros is almost a Greek cliché, full of beautiful whitewashed houses with bright blue doors lining cobblestoned streets on steep cliffs. Rugged and remote, without chain hotels or package tours — not even a bank or ATM — and accessible only by ferry, the volcanic island boasts solitude in spades, secluded beaches, and incredible sunsets.";
 
 
 
 })();
 
 (function () {
-	var name;
+	var Alonissos;
 
-	name = document.getElementById("name");
+	Alonissos = document.getElementById("Alonissos");
 
-	name.innerHTML = "Hanvinder Singh Rai";
+	Alonissos.innerHTML = "Off the mainland, this island, whose surrounding waters are a designated marine park full of seals and dolphins, is a nature lover's dream. The spectacular spot's crystal clear waters and beautiful beaches are perfect for diving, and its lush flora and dense forests ideal for hikes. Since the rich landscape is chock full of indigenous herbs, it's also known for its traditional medicine, and home of the International Academy of Homeopathy.";
+
+
+
+})();
+
+(function () {
+	var Spetses;
+
+	Spetses = document.getElementById("Spetses");
+
+	Spetses.innerHTML = "This bohemian paradise is more Côte d'Azur than Grecian island. Spetses was hot in the '60s, when it hosted Marilyn Monroe and Elizabeth Taylor, and remains a swanky summer retreat for Athens' glitterati to this day, full of fancy hotels, mansions and yachts, and stylish boutiques. Aside from a handful of locally-owned cabs, the four-mile-long island has banned cars in town, giving it a refreshingly 19th-century vibe, with horse-drawn carriages being a common sight. ";
+
+
+
+})();
+
+(function () {
+	var Amorgos;
+
+	Amorgos = document.getElementById("Amorgos");
+
+	Amorgos.innerHTML = "Shipwrecks, sea caves and beautiful clear waters make Amorgos popular with snorkelers, but the mountainous island is the perfect compromise between unwinding and adventure, boasting beautiful beaches but also hiking, scuba diving and rock-climbing. One of the island's main attractions is the 11th-century cliffside monastery of Panayia Hozoviótissa, which is precariously carved into a cliff.";
+
+
+
+})();
+
+(function () {
+	var Syros;
+
+	Syros = document.getElementById("Syros");
+
+	Syros.innerHTML = "A favourite Athenian escape and former shipowners' vacation spot full of grand mansions, Syros is little known to foreigners. Famous for its neoclassical architecture and perfectly preserved heritage, its capital, Ermoupolis, was a 13th-century Venetian-built town and important trade and industrial centre, and boasts giant churches and beautiful villas, a stunning town hall, and labyrinthine streets and stairways.";
+
+
+
+})();
+
+(function () {
+	var Milos;
+
+	Milos = document.getElementById("Milos");
+
+	Milos.innerHTML = "This volcanic island, with catacombs and ancient iron mines to explore, is dramatically rugged, and famous for its stunning rock formations. Often likened to a moonscape, it's also known for its hot springs, the ancient Venus De Milo statue that was found here, and for its diversity of incredible beaches. Known as &#34;the island of colours&#34; it's home to around 80 beaches — some only accessible by boat — ranging from stunningly white, to striking black, and even unusual red and grey. However, each beach has the same Evian-clear turquoise waters, and is surrounded by a rugged mountain landscape.";
+
+
+
+})();
+
+(function () {
+	var Hydra;
+
+	Hydra = document.getElementById("Hydra");
+
+	Hydra.innerHTML = "The first thing you'll notice on this beautiful island is the lack of cars — and buses, motorcycles, or other wheeled vehicles, as well as high rises. The winding little streets full of beautiful 18th-century mansions, churches, cathedrals, museums and art galleries are covered in cobblestones, and mostly trafficked by donkeys and humans. Back in the day, the island attracted celebrities like Leonard Cohen and Sophia Loren, but was somehow forgotten over the years. That means that travellers to Hydra can have the chic yet almost rural island paradise more or less to themselves. ";
+
+
+
+})();
+
+(function () {
+	var Ithaca;
+
+	Ithaca = document.getElementById("Ithaca");
+
+	Ithaca.innerHTML = "Most famous for being the home of Odysseus, and a prominent figure in Homer's &#34;The Odyssey,&#34;Ithaca is said to have been inhabited since the 2nd millennium BC. Made up of two islands joined by a narrow strip of land, many visit it to see the legendary sites mentioned by Homer. However, Ithaca is more than its mythical counterpart, beckoning with secluded beaches, dramatic cliffs, beautiful olive groves, and sleepy fishing villages full of Byzantine churches and monasteries.";
+
+
+
+})();
+
+(function () {
+	var Gavdos;
+
+	Gavdos = document.getElementById("Gavdos");
+
+	Gavdos.innerHTML = "Near the more well-known Crete, Gavdos is the most southern island in Greece — and the most southern spot in Europe discounting the Canaries. Only accessible by ferry, the remote island has only about 50 permanent residents, and can feel like your private playground. Local legend has it that the island was the home of goddess Calypso, who kept Odysseus prisoner here. Today, a favorite tourist activity is to visit the spot believed to be her cave. Be warned, you won't find any luxury hotels here. While the romantically under-developed, super laid back island has a number of rooms for rent, those are limited, as the real draw here for many is the free, seaside camping..";
 
 
 
