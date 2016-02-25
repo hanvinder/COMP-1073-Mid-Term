b804982bb654c6199da8b653b04fe4a4a3c7c4ff
hanvinder <hanvinder@gmail.com>
2016-02-25 09:21:13 -0500

    updated intro in js

--- Scripts/app.js
+++ Scripts/app.js
@@ -9,3 +9,40 @@ console.log("App Started...")
 
 })();
 
+/*app.js> Hanvinder Singh Rai> Portfolio> JavaScript for the first (about me ) page*/
+
+
+(function () {
+	var IntroParagraph;
+
+	IntroParagraph = document.getElementById("IntroParagraph");
+
+	IntroParagraph.innerHTML = "Now is the time to travel to Greece. The country may be going through an economic crisis, but many travellers say that it hasn't impacted the experience of visiting. Plus, there may be some great deals. <br>Greece has 1,400 islands, though only 230 of them are inhabited. <br>And while everyone knows about Santorini and Mykonos, there are gorgeous lesser-known islands in Greece, too.";
+
+
+
+})();
+
+(function () {
+	var thirdParagraph;
+
+	thirdParagraph = document.getElementById("thirdParagraph");
+
+	thirdParagraph.innerHTML = "Georgian College is really nice place to learn. They taught , design, and develop engaging and intuitive user interfaces. Courses cover the use of current technologies including but not limited to HTML, CSS, JavaScript, the Document Object Model (DOM), Application Programming Interfaces (APIs), and a variety of popular scripting frameworks.Morover, it's fun to learn here.";
+
+
+
+})();
+
+(function () {
+	var name;
+
+	name = document.getElementById("name");
+
+	name.innerHTML = "Hanvinder Singh Rai";
+
+
+
+})();
+
+
